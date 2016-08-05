3f49d579d4d9a0f472e21a168e2042b09c60fe3c
Richard Taylor <rtaylor@newoaksoftware.com>
2016-08-05 13:58:01 -0400

    Upgrade to .NET Core 1.0  and ASP.NET Core 1.0

--- angular2aspnetcore/src/angular2aspnetcore/app/app.ts
+++ angular2aspnetcore/src/angular2aspnetcore/app/app.ts
@@ -2,7 +2,7 @@
 
 @Component({
     selector: 'my-app',
-    template: '<h3>Angular 2.rc4 is running here!</h3>'
+    template: '<h3>Angular 2 RC4 is running here!</h3>'
 })
 
 export class AppComponent { }
