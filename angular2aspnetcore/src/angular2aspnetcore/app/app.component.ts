cedf1efcca751e78e5b32afabe19edc4132079bd
Richard Taylor <rtaylor@newoaksoftware.com>
2016-08-06 23:44:17 -0400

    Upgraded to version 3 beta 2 of the angular component router.  Removed the component entries in the precomplie array of the app component since there are no longer needed after the upgrade.

--- angular2aspnetcore/src/angular2aspnetcore/app/app.component.ts
+++ angular2aspnetcore/src/angular2aspnetcore/app/app.component.ts
@@ -1,14 +1,10 @@
 ﻿import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-import { SpeakerRegisterHomeComponent } from './home/speakerregisterhome.component';
-import { SpeakerListComponent } from './speakerlist/speakerlist.component';
-import { PageNotFoundComponent } from './pagenotfound.component';
 
 @Component({
     selector: 'my-app',
     templateUrl: '../app.component.html',
-    directives: [ROUTER_DIRECTIVES],
-    precompile: [SpeakerRegisterHomeComponent, SpeakerListComponent, PageNotFoundComponent]
+    directives: [ROUTER_DIRECTIVES]
 })
 
 export class AppComponent {
--- angular2aspnetcore/src/angular2aspnetcore/wwwroot/scripts/app.component.js
+++ angular2aspnetcore/src/angular2aspnetcore/wwwroot/scripts/app.component.js
@@ -10,9 +10,6 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var router_1 = require('@angular/router');
-var speakerregisterhome_component_1 = require('./home/speakerregisterhome.component');
-var speakerlist_component_1 = require('./speakerlist/speakerlist.component');
-var pagenotfound_component_1 = require('./pagenotfound.component');
 var AppComponent = (function () {
     function AppComponent() {
     }
@@ -20,8 +17,7 @@ var AppComponent = (function () {
         core_1.Component({
             selector: 'my-app',
             templateUrl: '../app.component.html',
-            directives: [router_1.ROUTER_DIRECTIVES],
-            precompile: [speakerregisterhome_component_1.SpeakerRegisterHomeComponent, speakerlist_component_1.SpeakerListComponent, pagenotfound_component_1.PageNotFoundComponent]
+            directives: [router_1.ROUTER_DIRECTIVES]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
