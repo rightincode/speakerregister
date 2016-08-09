a083f383d7e5fa4d1eef5a8b8dcafe0d077d2880
Richard L. Taylor Jr. <rtaylor@rightincode.com>
2016-08-09 11:36:52 -0400

    Removal of folder that should not be tracked in source control

--- angular2aspnetcore/src/angular2aspnetcore/wwwroot/app/home/speakerregisterhome.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    template: '<h3>Speaker Register Home Page</h3>'
-})
-
-export class SpeakerRegisterHomeComponent {}
--- angular2aspnetcore/src/angular2aspnetcore/wwwroot/app/pagenotfound.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    template: '<h3>Page Not Found</h3>'
-})
-
-export class PageNotFoundComponent { }
