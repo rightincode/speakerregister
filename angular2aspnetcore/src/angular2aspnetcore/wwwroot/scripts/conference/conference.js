8f710c344295eb8f0c1abca341353620718cdf6c
Richard L. Taylor Jr. <rtaylor@rightincode.com>
2016-10-08 09:11:25 -0400

    Folder reorganization

--- angular2aspnetcore/src/angular2aspnetcore/wwwroot/scripts/conference/conference.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Conference = (function () {
-    function Conference() {
-    }
-    return Conference;
-}());
-exports.Conference = Conference;
-//# sourceMappingURL=conference.js.map
