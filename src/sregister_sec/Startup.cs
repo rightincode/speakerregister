0da60cc39174525629e547fa4160996b4b44e6d6
Richard L. Taylor Jr. <rtaylor@rightincode.com>
2016-10-08 18:39:42 -0400

    webapi and sec project startup updates

--- src/sregister_sec/Startup.cs
+++ src/sregister_sec/Startup.cs
@@ -42,7 +42,7 @@ namespace sregister_sec
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World from sregister_sec!");
             });
         }
     }
