754754ecdb3715b85e25b155f4a6096340fdd38d
Richard L. Taylor Jr <rtaylor@rightincode.com>
2017-07-11 11:50:54 -0400

    Added support for the server to properly handle page refreshes from the browser hosting the angular app

--- src/sregister_web/Startup.cs
+++ src/sregister_web/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 
 namespace angular2aspnetcore
 {
@@ -14,6 +15,19 @@ namespace angular2aspnetcore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            // Route all unknown requests to app root
+            app.Use(async (context, next) =>
+            {
+                await next();
+                                
+                if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
+                {
+                    context.Request.Path = "/index.html";
+                    context.Response.StatusCode = 200; 
+                    await next();
+                }
+            });
+
             app.UseFileServer();
         }
     }
