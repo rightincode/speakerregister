1e9c7b91da2aa09ad72c3ec36aed5b0b4de64f1c
Richard Taylor <rtaylor@rightincode.com>
2017-09-11 23:43:22 -0400

    Added core test project and a basic set of tests for the speaker class

---
+++ src/sregister_core.Tests/SpeakerTests.cs
@@ -0,0 +1,216 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using sregister_core.Models;
+
+namespace sregister_core.Tests
+{
+    [TestClass]
+    public class SpeakerTests
+    {
+        [TestMethod]
+        public void ValidationImplemented()
+        {
+            var testSpeaker = new Speaker
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Address1 = "Address 1",
+                Address2 = "Address 2",
+                City = "City",
+                State = "NC",
+                Zipcode = "11111",
+                PhoneNumber = "7777777777",
+                EmailAddress = "email@email.com",
+                Id = 1
+            };
+
+            try
+            {
+                testSpeaker.Validate(new ValidationContext(testSpeaker));
+            }
+            catch(NotImplementedException ex)
+            {
+                Assert.Fail("Received exception when no exception expected.  Exception: " + ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void SpeakerFirstNameMissing()
+        {
+            var testSpeaker = new Speaker
+            {
+                FirstName = "",
+                LastName = "LastName",
+                Address1 = "Address 1",
+                Address2 = "Address 2",
+                City = "City",
+                State = "NC",
+                Zipcode = "11111",
+                PhoneNumber = "7777777777",
+                EmailAddress = "email@email.com",
+                Id = 1
+            };
+
+            IEnumerable<ValidationResult> speakerValidationResults = testSpeaker.Validate(new ValidationContext(testSpeaker));
+
+            Assert.AreEqual(1, speakerValidationResults.Where((result) => result.MemberNames.Contains("FirstName")).Count(), "First name should be missing.");
+        }
+
+        [TestMethod]
+        public void SpeakerLastNameMissing()
+        {
+            var testSpeaker = new Speaker
+            {
+                FirstName = "FirstName",
+                LastName = "",
+                Address1 = "Address 1",
+                Address2 = "Address 2",
+                City = "City",
+                State = "NC",
+                Zipcode = "11111",
+                PhoneNumber = "7777777777",
+                EmailAddress = "email@email.com",
+                Id = 1
+            };
+
+            IEnumerable<ValidationResult> speakerValidationResults = testSpeaker.Validate(new ValidationContext(testSpeaker));
+
+            Assert.AreEqual(1, speakerValidationResults.Where((result) => result.MemberNames.Contains("LastName")).Count(), "Last name should be missing.");
+        }
+
+        [TestMethod]
+        public void SpeakerEmailAddressMissing()
+        {
+            var testSpeaker = new Speaker
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Address1 = "Address 1",
+                Address2 = "Address 2",
+                City = "City",
+                State = "NC",
+                Zipcode = "11111",
+                PhoneNumber = "7777777777",
+                EmailAddress = "",
+                Id = 1
+            };
+
+            IEnumerable<ValidationResult> speakerValidationResults = testSpeaker.Validate(new ValidationContext(testSpeaker));
+
+            Assert.AreEqual(1, speakerValidationResults.Where((result) => result.MemberNames.Contains("EmailAddress")).Count(), "Email address should be missing.");
+        }
+
+        [TestMethod]
+        public void SpeakerPhoneNumberMissing()
+        {
+            var testSpeaker = new Speaker
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Address1 = "Address 1",
+                Address2 = "Address 2",
+                City = "City",
+                State = "NC",
+                Zipcode = "11111",
+                PhoneNumber = "",
+                EmailAddress = "email@email.com",
+                Id = 1
+            };
+
+            IEnumerable<ValidationResult> speakerValidationResults = testSpeaker.Validate(new ValidationContext(testSpeaker));
+
+            Assert.AreEqual(1, speakerValidationResults.Where((result) => result.MemberNames.Contains("PhoneNumber")).Count(), "Phone number should be missing.");
+        }
+
+        [TestMethod]
+        public void SpeakerAddress1Missing()
+        {
+            var testSpeaker = new Speaker
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Address1 = "",
+                Address2 = "Address 2",
+                City = "City",
+                State = "NC",
+                Zipcode = "11111",
+                PhoneNumber = "7777777777",
+                EmailAddress = "email@email.com",
+                Id = 1
+            };
+
+            IEnumerable<ValidationResult> speakerValidationResults = testSpeaker.Validate(new ValidationContext(testSpeaker));
+
+            Assert.AreEqual(1, speakerValidationResults.Where((result) => result.MemberNames.Contains("Address1")).Count(), "Address1 should be missing.");
+        }
+
+        [TestMethod]
+        public void SpeakerCityMissing()
+        {
+            var testSpeaker = new Speaker
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Address1 = "Address 1",
+                Address2 = "Address 2",
+                City = "",
+                State = "NC",
+                Zipcode = "11111",
+                PhoneNumber = "7777777777",
+                EmailAddress = "email@email.com",
+                Id = 1
+            };
+
+            IEnumerable<ValidationResult> speakerValidationResults = testSpeaker.Validate(new ValidationContext(testSpeaker));
+
+            Assert.AreEqual(1, speakerValidationResults.Where((result) => result.MemberNames.Contains("City")).Count(), "City should be missing.");
+        }
+
+        [TestMethod]
+        public void SpeakerStateMissing()
+        {
+            var testSpeaker = new Speaker
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Address1 = "Address 1",
+                Address2 = "Address 2",
+                City = "City",
+                State = "",
+                Zipcode = "11111",
+                PhoneNumber = "7777777777",
+                EmailAddress = "email@email.com",
+                Id = 1
+            };
+
+            IEnumerable<ValidationResult> speakerValidationResults = testSpeaker.Validate(new ValidationContext(testSpeaker));
+
+            Assert.AreEqual(1, speakerValidationResults.Where((result) => result.MemberNames.Contains("State")).Count(), "State should be missing.");
+        }
+
+        [TestMethod]
+        public void SpeakerZipCodeMissing()
+        {
+            var testSpeaker = new Speaker
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Address1 = "Address 1",
+                Address2 = "Address 2",
+                City = "City",
+                State = "NC",
+                Zipcode = "",
+                PhoneNumber = "7777777777",
+                EmailAddress = "email@email.com",
+                Id = 1
+            };
+
+            IEnumerable<ValidationResult> speakerValidationResults = testSpeaker.Validate(new ValidationContext(testSpeaker));
+
+            Assert.AreEqual(1, speakerValidationResults.Where((result) => result.MemberNames.Contains("Zipcode")).Count(), "Zipcode should be missing.");
+        }
+    }
+}
--- src/sregister_core/Models/Speaker.cs
+++ src/sregister_core/Models/Speaker.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace sregister_core.Models
 {
-    public class Speaker
+    public class Speaker : IValidatableObject
     {
         [Required]
         public int Id { get; set; }
@@ -32,5 +34,62 @@ namespace sregister_core.Models
 
         [Required]
         public string Zipcode { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var results = new List<ValidationResult>();
+            results.AddRange(ValidateRequiredFields());
+
+            return results;
+        }
+
+        private IEnumerable<ValidationResult> ValidateRequiredFields()
+        {
+            var memberNameList = new List<string>();
+
+            //no need to validate Id, int32 will be initialized
+
+            if (string.IsNullOrEmpty(this.FirstName) || string.IsNullOrWhiteSpace(this.FirstName))
+            {
+                memberNameList.Add("FirstName");
+            }
+
+            if (string.IsNullOrEmpty(this.LastName) || string.IsNullOrWhiteSpace(this.LastName))
+            {
+                memberNameList.Add("LastName");
+            }
+
+            if (string.IsNullOrEmpty(this.EmailAddress) || string.IsNullOrWhiteSpace(this.EmailAddress))
+            {
+                memberNameList.Add("EmailAddress");
+            }
+
+            if (string.IsNullOrEmpty(this.PhoneNumber) || string.IsNullOrWhiteSpace(this.PhoneNumber))
+            {
+                memberNameList.Add("PhoneNumber");
+            }
+
+            if (string.IsNullOrEmpty(this.Address1) || string.IsNullOrWhiteSpace(this.Address1))
+            {
+                memberNameList.Add("Address1");
+            }
+
+            if (string.IsNullOrEmpty(this.City) || string.IsNullOrWhiteSpace(this.City))
+            {
+                memberNameList.Add("City");
+            }
+
+            if (string.IsNullOrEmpty(this.State) || string.IsNullOrWhiteSpace(this.State))
+            {
+                memberNameList.Add("State");
+            }
+
+            if (string.IsNullOrEmpty(this.Zipcode) || string.IsNullOrWhiteSpace(this.Zipcode))
+            {
+                memberNameList.Add("Zipcode");
+            }
+
+            yield return new ValidationResult("Required Field Missing", memberNameList);
+        }
     }
 }
