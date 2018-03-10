4771ee227e5a71d36e66c51f66a85218b6315771
Richard L. Taylor Jr <rtaylor@rightincode.com>
2018-03-10 14:48:24 -0800

    Set mvc compatibility version to 2.1

--- src/sregister_webapi/Startup.cs
+++ src/sregister_webapi/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -27,7 +28,7 @@ namespace sregister_webapi
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddCors();
             services.AddOptions();
